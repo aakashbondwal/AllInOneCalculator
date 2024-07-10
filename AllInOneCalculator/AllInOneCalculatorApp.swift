e116d1d3ab65002359827be619902ba840e75131
Aakash Bondwal <94138250+aakashbondwal@users.noreply.github.com>
2024-07-10 19:57:17 +0530

    Initial Commit

---
+++ AllInOneCalculator/AllInOneCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllInOneCalculatorApp.swift
+//  AllInOneCalculator
+//
+//  Created by Aakash  Bondwal  on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AllInOneCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
